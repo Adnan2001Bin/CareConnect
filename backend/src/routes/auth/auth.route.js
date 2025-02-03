e4e0812d6705a9704970fc67a9e5834de220f341
Adnan Bin Hossen <hossen15-4543@diu.edu.bd>
2025-02-03 18:45:50 +0600

    backend for auth

--- backend/src/routes/auth/auth.route.js
+++ backend/src/routes/auth/auth.route.js
@@ -1,10 +1,26 @@
 import express from "express";
-import { registerUser } from "../../controllers/auth/controllers.js";
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+  sendVerifyOtp,
+  verifyEmail,
+  isAuthenticated,
+  resetPassword,
+  sendResetOtp,
+} from "../../controllers/auth/controllers.js";
 
+import authMiddleware from "../../controllers/authMiddleware.js";
 
-const authRouter = express.Router()
+const authRouter = express.Router();
 
 // Public routes
-authRouter.post("/register" , registerUser)
-
+authRouter.post("/register", registerUser);
+authRouter.post("/login", loginUser);
+authRouter.post("/logout", logoutUser);
+authRouter.post("/send-verify-otp", authMiddleware, sendVerifyOtp);
+authRouter.post("/verify-account", authMiddleware , verifyEmail);
+authRouter.get("/is-auth", authMiddleware, isAuthenticated);
+authRouter.post("/send-reset-otp", sendResetOtp);
+authRouter.post("/reset-password", resetPassword);
 export default authRouter;
