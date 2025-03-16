a10bce15242ba32dcb7e1ab04d94f01060c00458
Adnan Bin Hossen <hossen15-4543@diu.edu.bd>
2025-03-16 15:31:07 +0600

    added Highlighted Doctors section

--- frontend/src/components/user-view/TopDoctors.jsx
+++ frontend/src/components/user-view/TopDoctors.jsx
@@ -1,19 +1,38 @@
 
 import { doctors } from "@/Config";
-import React from "react";
+import { fetchAlldoctors } from "@/store/Admin/Doctor-Slice";
+import { fetchAllFilterDoctors } from "@/store/Paitent/patient-slice";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 
 const TopDoctors = () => {
     const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const { doctorList, loading } = useSelector(
+      (state) => state.paitentViewDoctor
+    );
+
+    useEffect(() => {
+      // Initial fetch with no filters
+      dispatch(
+        fetchAllFilterDoctors({
+          filterParams: { speciality: "" },
+        })
+      );
+    }, [dispatch]);
+
+      console.log(doctorList);
+      
   return (
     <div className="flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10">
-      <h1 className="text-3xl font-medium">Top Doctors to Book</h1>
+      <h1 className="text-3xl font-medium">Our Highlighted Doctors</h1>
       <p className="sm:w-1/3 text-center text-sm">
         Simply browse through our extensive list of trusted doctors.
       </p>
       <div className="w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0">
-        {doctors.slice(0, 10).map((item, index) => (
+        {doctorList.slice(0, 10).map((item, index) => (
           <div onClick={()=>navigate(`/appoinments/${item._id}`)} className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500">
             <img className="bg-blue-50" src={item.image} alt="" />
             <div className="p-4">
@@ -27,7 +46,7 @@ const TopDoctors = () => {
           </div>
         ))}
       </div>
-      <button className="bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10">
+      <button onClick={()=>navigate('/doctorslist')} className="bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10">
         more
       </button>
     </div>
